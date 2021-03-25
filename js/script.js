343eba8b14c12732a945a64b8b3f930f1c1da0d8
leenay123 <81352931+leenay123@users.noreply.github.com>
2021-03-25 17:48:07 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+﻿let sImg_1=document.querySelector('.s1');
+let sImg_2=document.querySelector('.s2');
+let sImg_3=document.querySelector('.s3');
+let sAry=[s1,s2,s3];
+sAry[0].style.display="block";
+
+let sBtn_L=document.querySelector('.left2');
+let sBtn_R=document.querySelector('.right2');
+
+let imgIndex=0;
+let imgCnt=(sAry.length)-1;
+
+
+
+		
+		
